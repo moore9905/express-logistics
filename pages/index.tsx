fe8819fae52ca1f9aac18968d19d207e41eeac5c
Musa Jubril <jewbreel1@gmail.com>
2024-07-02 22:46:48 +0100

    fixed typo error

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function index() {
         backgroundImage: `url(/bg.jpg)`
     }}>
         <div className="text-5xl text-center">Welcome to <span className='font-extrabold'>EXPRESS LOGISTIC SERVICE</span></div>
-        <div className="text-center px-3">to start tracking youur items click on get started and input your tracking code</div>
+        <div className="text-center px-3">to start tracking your items click on get started and input your tracking code</div>
         <div className="flex justify-center max-w-xs w-full">
             <Link href='/login' className="px-12 py-3 rounded-md text-white bg-[#4d47c3] border-2 border-[#4d47c3] hover:bg-white transition-all duration-500 hover:scale-105 transform hover:text-[#4d47c3] cursor-pointer">Get Started</Link>
         </div>
